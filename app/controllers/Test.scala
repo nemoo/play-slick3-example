d8b249a5d9af07bea09a517309317efd800cdc8b
Fredrik Ekholdt <freekh@gmail.com>
2014-02-04 21:45:39 +0100

    indents (also changed the values but that shouldnt matter)

--- app/controllers/Test.scala
+++ app/controllers/Test.scala
@@ -1,32 +1,31 @@
 package controllers
 
-import play.api.mvc._
+import play.api.mvc.Controller
 import org.joda.time.DateTime
 import play.api.libs.json._
 
 import play.api.db.slick._
 
 import play.api.db.slick.DBAction
-
 import modelsg.current.activityRepo
 import modelsg.current.activityRepo.profile.simple._
 
 object Test extends Controller {
-  import play.api.Play.current
 
   def test1 = DBAction { implicit rs =>
-	  val data = activityRepo.findByName("assf").firstOption
-    //	var data = activityRepo.findByName("xy").firstOption
-	  
-    Ok(data.toString)
-  }
+    
+	val data = activityRepo.findByName("xzy").firstOption
+//	var data = activityRepo.findByName("xy").firstOption
+	
+    Ok(data.toString)     
+  }  
   
   def test2 = DBAction { implicit rs =>
-    val data = activityRepo.findByName("xd").firstOption
-    //	var data = activityRepo.findByName("xy").firstOption
-	  
+    
+	val data = activityRepo.findByName("ysd").firstOption
+//	var data = activityRepo.findByName("xy").firstOption
+	
     Ok(data.toString)
-
-  }
+  }    
 
 }
