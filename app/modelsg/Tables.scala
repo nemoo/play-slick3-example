6f2cf53d2cbda3d12341bebb1eff6d3bb3ca8688
Jochen Kapaun <jokap@yahoo.com>
2014-02-09 21:38:22 +0100

    Revert "use typed ids"
    This reverts commit 647cb4ec3ed60fa1ba6f5a856a27db84f53278e0.

--- app/modelsg/Tables.scala
+++ app/modelsg/Tables.scala
@@ -2,19 +2,12 @@ package modelsg
 
 import play.api.db.slick.Profile
 
-
 /** Entity class storing rows of table Activities
  *  @param id Database column ID AutoInc, PrimaryKey
  *  @param name Database column NAME 
  *    */
-
-
-class ActivityId(val value: Int) extends AnyVal
-case class Activity(id: ActivityId, name: String)
-
-class ItemId(val value: Int) extends AnyVal
-case class Item(id: ItemId, color: String)
-
+case class Activity(id: Int, name: String)
+case class Item(id: Int, color: String)
 
 /** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
 trait Tables { this: Profile =>
@@ -23,8 +16,6 @@ trait Tables { this: Profile =>
   import scala.slick.model.ForeignKeyAction
   import scala.slick.jdbc.{GetResult => GR}
   // NOTE: GetResult mappers for plain SQL are only generated for tables where Slick knows how to map the types of all columns.
-  implicit def activityIdType = MappedColumnType.base[ActivityId, Int](_.value, new ActivityId(_))
-  implicit def itemIdType = MappedColumnType.base[ItemId, Int](_.value, new ItemId(_))
   
  /** Table description of table ACTIVITY. Objects of this class serve as prototypes for rows in queries. */
   class Activities(tag: Tag) extends Table[Activity](tag, "ACTIVITY") {
@@ -33,7 +24,7 @@ trait Tables { this: Profile =>
     def ? = (id.?, name.?).shaped.<>({r=>import r._; _1.map(_=> Activity.tupled((_1.get, _2.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
     
     /** Database column ID AutoInc, PrimaryKey */
-    val id: Column[ActivityId] = column[ActivityId]("ID", O.AutoInc, O.PrimaryKey)
+    val id: Column[Int] = column[Int]("ID", O.AutoInc, O.PrimaryKey)
     /** Database column ATYPE  */
     val name: Column[String] = column[String]("NAME")
     
@@ -45,7 +36,7 @@ trait Tables { this: Profile =>
     def ? = (id.?, color.?).shaped.<>({r=>import r._; _1.map(_=> Item.tupled((_1.get, _2.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
     
     /** Database column ID AutoInc, PrimaryKey */
-    val id: Column[ItemId] = column[ItemId]("ID", O.AutoInc, O.PrimaryKey)
+    val id: Column[Int] = column[Int]("ID", O.AutoInc, O.PrimaryKey)
     /** Database column COLOR  */
     val color: Column[String] = column[String]("COLOR")
     
